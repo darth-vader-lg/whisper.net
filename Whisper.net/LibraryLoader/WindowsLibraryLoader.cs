b8bcb6da0b0d05521df71e97534a58913f67dd39
darth-vader-lg <luigi.generale@gmail.com>
2023-04-11 01:42:07 +0200

    Runtime autoselection of the avx arch to run on old CPUs: - Submodule whisper.cpp pointing to dart-vader-lg forked repository - New runtimes for whisper.cpp library whit avx1 support and no-avx.

--- Whisper.net/LibraryLoader/WindowsLibraryLoader.cs
+++ Whisper.net/LibraryLoader/WindowsLibraryLoader.cs
@@ -7,11 +7,47 @@ namespace Whisper.net.LibraryLoader;
 
 internal class WindowsLibraryLoader : ILibraryLoader
 {
+    private delegate IntPtr whisper_print_system_info_type();
+
     public LoadResult OpenLibrary(string filename)
     {
-        var loadedLib = LoadLibrary(filename);
+        var (module, info) = LoadLibraryWithInfo(filename);
 
-        if (loadedLib == IntPtr.Zero)
+        if (module == IntPtr.Zero)
+        {
+            var errorCode = Marshal.GetLastWin32Error();
+            var errorMessage = new Win32Exception(errorCode).Message;
+            return LoadResult.Failure(errorMessage);
+        }
+
+        if (info.ToLowerInvariant().Contains("avx2 = 1"))
+        {
+            return LoadResult.Success;
+        }
+
+        FreeLibrary(module);
+
+        var avx1Filename = Path.Combine(Path.GetDirectoryName(filename) + "-avx1", Path.GetFileName(filename));
+        (module, info) = LoadLibraryWithInfo(avx1Filename);
+
+        if (module == IntPtr.Zero)
+        {
+            var errorCode = Marshal.GetLastWin32Error();
+            var errorMessage = new Win32Exception(errorCode).Message;
+            return LoadResult.Failure(errorMessage);
+        }
+
+        if (info.ToLowerInvariant().Contains("avx = 1"))
+        {
+            return LoadResult.Success;
+        }
+
+        FreeLibrary(module);
+
+        var noavxFilename = Path.Combine(Path.GetDirectoryName(filename) + "-noavx", Path.GetFileName(filename));
+        module = LoadLibrary(noavxFilename);
+
+        if (module == IntPtr.Zero)
         {
             var errorCode = Marshal.GetLastWin32Error();
             var errorMessage = new Win32Exception(errorCode).Message;
@@ -21,6 +57,34 @@ internal class WindowsLibraryLoader : ILibraryLoader
         return LoadResult.Success;
     }
 
+
+    private (IntPtr module, string info) LoadLibraryWithInfo(string filename)
+    {
+        if (LoadLibrary(filename) is var module && module == IntPtr.Zero)
+        {
+            return default;
+        }
+
+        var procAddr = GetProcAddress(module, "whisper_print_system_info");
+        if (procAddr == IntPtr.Zero)
+        {
+            FreeLibrary(module);
+            return default;
+        }
+
+        var whisper_print_system_info = (whisper_print_system_info_type)Marshal.GetDelegateForFunctionPointer(procAddr, typeof(whisper_print_system_info_type));
+        var strInfo = Marshal.PtrToStringAnsi(whisper_print_system_info());
+
+        return (module, strInfo);
+    }
+
+    [DllImport("kernel32", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)]
+    public static extern IntPtr GetProcAddress(IntPtr module, string proc);
+
     [DllImport("kernel32", SetLastError = true, CharSet = CharSet.Auto)]
     private static extern IntPtr LoadLibrary([MarshalAs(UnmanagedType.LPTStr)] string lpFileName);
+
+    [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
+    private static extern bool FreeLibrary(IntPtr hModule);
+
 }
